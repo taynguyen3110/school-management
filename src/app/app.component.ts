f6d759430bdf81c1e55cd6007f7115471e2a016d
taynguyen3110 <taynguyen3110@gmail.com>
2025-01-06 15:49:21 +1100

    fix: hide secret key

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { StudentsComponent } from './students/student-page/students-page.component';
@@ -6,21 +6,34 @@ import { HeaderComponent } from './shared/components/header/header.component';
 import { LoginComponent } from './auth/login-page.component';
 import { AuthService } from './shared/services/auth.service';
 import { LoadingSpinnerComponent } from './shared/components/loading-spinner/loading-spinner.component';
+import { GoogleMapsLoaderService } from './shared/services/google-place-api-loader.service';
 
 @Component({
-    selector: 'sman-root',
-    imports: [
-        RouterOutlet,
-        StudentsComponent,
-        HeaderComponent,
-        FontAwesomeModule,
-        LoginComponent,
-        LoadingSpinnerComponent,
-    ],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  selector: 'sman-root',
+  imports: [
+    RouterOutlet,
+    StudentsComponent,
+    HeaderComponent,
+    FontAwesomeModule,
+    LoginComponent,
+    LoadingSpinnerComponent,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'school-management';
-  constructor(public authService: AuthService) {}
+  constructor(
+    public authService: AuthService,
+    private googleMapsLoader: GoogleMapsLoaderService
+  ) {}
+
+  ngOnInit(): void {
+    this.googleMapsLoader
+      .loadGoogleMaps()
+      .then(() => {})
+      .catch((error) => {
+        console.error('Error loading Google Maps script:', error);
+      });
+  }
 }
