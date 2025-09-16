b6943b80f4c10b65a3fa611febe527018258fdb6
taynguyen3110 <taynguyen3110@gmail.com>
2025-09-16 14:04:50 +1000

    fix: use ngrok for tunnel request

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: (window as any).env?.API_URL || "http://13.54.63.78:3001/",
+  apiUrl: (window as any).env?.API_URL || "https://a3bcc4456ec5.ngrok-free.app",
   // 'https://school-mgmt-api-gkqr.onrender.com'
   googleMapsApiKey: (window as any).env?.GOOGLE_MAPS_API_KEY || 'AIzaSyALrnz4qQpv0DC1tMwSAwq7Jd12dc2e9vE',
   env: 'production' as 'development' | 'production'
