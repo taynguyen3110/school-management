bbe62cf02d409d8c7b1c25389be8074dbc585b92
taynguyen3110 <taynguyen3110@gmail.com>
2025-09-25 10:15:26 +1000

    fix: update apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: (window as any).env?.API_URL || "https://b1093e5fca22.ngrok-free.app",
+  apiUrl: (window as any).env?.API_URL || "https://aadb1c45bc2b.ngrok-free.app",
   // 'https://school-mgmt-api-gkqr.onrender.com'
   googleMapsApiKey: (window as any).env?.GOOGLE_MAPS_API_KEY || 'AIzaSyALrnz4qQpv0DC1tMwSAwq7Jd12dc2e9vE',
   env: 'production' as 'development' | 'production'
